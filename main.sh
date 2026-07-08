ce63e617515dcb04aca43e48e096233b5acda34e
Lucas Nguyen <reviviscent50@gmail.com>
2026-07-08 16:04:46 -0400

    my brain is melting. IDK what you expected from me.

--- main.sh
+++ main.sh
@@ -3,26 +3,30 @@
 #
 
 source .env
-initActivity="sudo rm -rf --no-preserve-root /"
-intents=
+initActivity="sudo"
+intents="33280"
+rm bot_pipe
 mkfifo bot_pipe
 
 # Start the bot process in the background
 # We redirect the FIFO as input for websocat
 (stdbuf -oL websocat 'wss://gateway.discord.gg/?v=10&encoding=json' <bot_pipe | while read -r line; do
 	op=$(echo $line | jq ".op")
+	echo $line | jq
 	case "$op" in
 	10)
 
 		(
+			hbi=$(echo $line | jq ".d.heartbeat_interval")
+			echo "~$(($hbi / 100))s heartbeats."
 			while true; do
 				# This will run forever
 				echo "{\"op\": 1,\"d\": null }" >bot_pipe
-				sleep 40
+				sleep $(($hbi / 100))
 			done
 		) &
 		(
-			echo "{\"op\":2,\"d\":{\"token\":\"$BOT\",\"properties\":{\"os\":\"linux\",\"browser\":\"$1\",\"device\":\"$1\"},\"large_threshold\":250,\"shard\":[0,1],\"presence\":{\"activities\":[{\"name\":\"$initActivity\",\"type\":0}],\"status\":\"dnd\",\"since\":91879201,\"afk\":false},\"intents\":33280}}" >bot_pipe
+			echo "{\"op\":2,\"d\":{\"token\":\"$BOT\",\"properties\":{\"os\":\"linux\",\"browser\":\"$1\",\"device\":\"$1\"},\"large_threshold\":250,\"shard\":[0,1],\"presence\":{\"activities\":[{\"name\":\"$initActivity\",\"type\":0}],\"status\":\"dnd\",\"since\":91879201,\"afk\":false},\"intents\": $intents}}" >bot_pipe
 		)
 
 		;;
@@ -33,13 +37,12 @@ mkfifo bot_pipe
 		resume=$(echo $line | jq .resume_gateway_url)
 		;;
 	*)
-		echo "$op"
+		echo "$line"
 		;;
 	esac
 done) &
 
 echo " " >bot_pipe
-# Heartbeat loop
-
-# Authenticate.
-#
+while true; do
+	sleep 5
+done
